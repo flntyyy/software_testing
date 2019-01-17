75e4700de49dc43e155f30c7a4ff7f0f1319b1ff
alexandrsmirnov <flntyyy@gmail.com>
2019-01-17 23:10:55 +0300

    Написал прямоугольник

--- sandbox/src/main/java/MyClass.java
+++ sandbox/src/main/java/MyClass.java
@@ -1,5 +1,6 @@
 public class MyClass {
     public static void main(String[] args) {
-System.out.println("12345");
+        Rectangle r = new Rectangle(6,8);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и "+ r.b + " = " + r.area());
     }
 }
