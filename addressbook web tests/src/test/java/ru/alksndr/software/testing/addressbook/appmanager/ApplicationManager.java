ecf115fa68003d4a98f683dc01c2f7f24f50ba1e
alexandrsmirnov <flntyyy@gmail.com>
2019-01-20 22:53:00 +0300

    Оптимизированны некторые функции

--- addressbook web tests/src/test/java/ru/alksndr/software/testing/addressbook/appmanager/ApplicationManager.java
+++ addressbook web tests/src/test/java/ru/alksndr/software/testing/addressbook/appmanager/ApplicationManager.java
@@ -2,7 +2,10 @@
 package ru.alksndr.software.testing.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,14 +16,25 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
-
-    private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        System.setProperty("webdriver.gecko.driver", "drivers/chromedriver");
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        if (browser.equals(BrowserType.GOOGLECHROME)) {
+            driver = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            driver = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.SAFARI)) {
+            driver = new SafariDriver();
+        }
+
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://localhost/addressbook/index.php");
         groupHelper = new GroupHelper(driver);
         navigationHelper = new NavigationHelper(driver);
