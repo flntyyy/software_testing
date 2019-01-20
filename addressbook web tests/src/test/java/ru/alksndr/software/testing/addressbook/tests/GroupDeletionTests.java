40256523c6a3ddc54497285eee3cd9e1112c84a0
alexandrsmirnov <flntyyy@gmail.com>
2019-01-20 13:23:13 +0300

    Добавлен тест на модификацию групп

--- addressbook web tests/src/test/java/ru/alksndr/software/testing/addressbook/tests/GroupDeletionTests.java
+++ addressbook web tests/src/test/java/ru/alksndr/software/testing/addressbook/tests/GroupDeletionTests.java
@@ -4,7 +4,6 @@ import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
 
-
     @Test
     public void testGroupDeletion() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
