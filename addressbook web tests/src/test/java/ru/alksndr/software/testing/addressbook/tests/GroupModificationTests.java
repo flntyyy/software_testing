1b70019cd63ebc65cc66b966df7750c66180f0e6
alexandrsmirnov <flntyyy@gmail.com>
2019-01-21 22:24:04 +0300

    Update GroupModificationTests.java

--- addressbook web tests/src/test/java/ru/alksndr/software/testing/addressbook/tests/GroupModificationTests.java
+++ addressbook web tests/src/test/java/ru/alksndr/software/testing/addressbook/tests/GroupModificationTests.java
@@ -6,14 +6,14 @@ import ru.alksndr.software.testing.addressbook.model.GroupData;
 public class GroupModificationTests extends TestBase {
 
     @Test
-    public void testGroupModification () {
+    public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
         if (!app.getGroupHelper().isThereAGroup()) {
-            app.getGroupHelper().createGroup (new GroupData("test1","test122",null));
+            app.getGroupHelper().createGroup(new GroupData("test1", "test122", null));
         }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1","321","456"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "321", "456"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
