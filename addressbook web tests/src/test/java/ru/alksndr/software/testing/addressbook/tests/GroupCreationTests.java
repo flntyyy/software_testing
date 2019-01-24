bc89ae4cfaf1beddeb4eec40035444f0faa275cd
alexandrsmirnov <flntyyy@gmail.com>
2019-01-24 23:36:43 +0300

    Добавлены проверки объектов групдат

--- addressbook web tests/src/test/java/ru/alksndr/software/testing/addressbook/tests/GroupCreationTests.java
+++ addressbook web tests/src/test/java/ru/alksndr/software/testing/addressbook/tests/GroupCreationTests.java
@@ -4,16 +4,25 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.alksndr.software.testing.addressbook.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
-        int after = app.getGroupHelper().getGroupCount();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        GroupData group = new GroupData("Test1", null, null);
+        app.getGroupHelper().createGroup(group);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
 
-        Assert.assertEquals(after, before+1);
+
+        group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 }
