3569728bd103c2a566a975100fb61f64fced61a0
alexandrsmirnov <flntyyy@gmail.com>
2019-01-26 13:44:21 +0300

    Улучшена читаемость

--- addressbook web tests/src/test/java/ru/alksndr/software/testing/addressbook/tests/GroupCreationTests.java
+++ addressbook web tests/src/test/java/ru/alksndr/software/testing/addressbook/tests/GroupCreationTests.java
@@ -5,24 +5,23 @@ import org.testng.annotations.Test;
 import ru.alksndr.software.testing.addressbook.model.GroupData;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
-        app.getNavigationHelper().gotoGroupPage();
-        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.goTo().groupPage();
+        List<GroupData> before = app.group().list();
         GroupData group = new GroupData("Test1", null, null);
-        app.getGroupHelper().createGroup(group);
-        List<GroupData> after = app.getGroupHelper().getGroupList();
+        app.group().create(group);
+        List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-
-        group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
-
 }
